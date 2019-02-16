acb6cfb9e516c7757d38a48eaa522238618ea017
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-16 11:48:59 +0200

    Move array insertion functions to separate header file

---
+++ array_insert.h
@@ -0,0 +1,25 @@
+#ifndef ALCOTRIP_ARRAY_INSERT_H
+#define ALCOTRIP_ARRAY_INSERT_H
+
+template<class T>
+void insert_subarray_into_array(T array[], int array_size, int insert_index, T sub_array[], int subarray_size);
+
+template<class T>
+void shift_array_elements_by(T array[], int array_size, int shift_by, int start_from);
+
+template<class T>
+void insert_subarray_into_array(T array[], int array_size, int insert_index, T sub_array[], int subarray_size) {
+    shift_array_elements_by(array, array_size, subarray_size, insert_index);
+    for (int i = insert_index, k = 0; i < insert_index + subarray_size && k < subarray_size; i++, k++) {
+        array[i] = sub_array[k];
+    }
+}
+
+template<class T>
+void shift_array_elements_by(T array[], int array_size, int shift_by, int start_from) {
+    for (int i = array_size; i > start_from; i--) {
+        array[i] = array[i - shift_by];
+    }
+}
+
+#endif //ALCOTRIP_ARRAY_INSERT_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-
-template<class T>
-void insert_subarray_into_array(T array[], int array_size, int insert_index, T sub_array[], int subarray_size);
-
-template<class T>
-void shift_array_elements_by(T array[], int array_size, int shift_by, int start_from);
+#include "array_insert.h"
 
 template<class T>
 void print_array(T array[], int array_size);
@@ -18,21 +13,6 @@ int main() {
     std::cout << "Array after insert: " << std::endl;
     print_array(array, array_size);
     return 0;
-}
-
-template<class T>
-void insert_subarray_into_array(T array[], int array_size, int insert_index, T sub_array[], int subarray_size) {
-    shift_array_elements_by(array, array_size, subarray_size, insert_index);
-    for (int i = insert_index, k = 0; i < insert_index + subarray_size && k < subarray_size; i++, k++) {
-        array[i] = sub_array[k];
-    }
-}
-
-template<class T>
-void shift_array_elements_by(T array[], int array_size, int shift_by, int start_from) {
-    for (int i = array_size; i > start_from; i--) {
-        array[i] = array[i - shift_by];
-    }
 }
 
 // Utility functions

c3bf842a5d39fd7535914296439daada2352a9cf
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-18 20:17:15 +0200

    Add expected result of a function call to print stream

--- main.cpp
+++ main.cpp
@@ -8,17 +8,17 @@
 int main() {
     int array[] = {1, 2, 3, 4, 5};
     int array_size = std::size(array);
-    std::cout << "Array before insert: " << std::endl;
+    std::cout << "Array before insert(Should be: [1, 2, 3, 4, 5]): " << std::endl;
     print_array(array, array_size);
     insert_subarray_into_array(array, array_size, 1, new int[2]{42, 89}, 2);
-    std::cout << "Array after insert: " << std::endl;
+    std::cout << "Array after insert(Should be: [1, 42, 89, 2, 3]): " << std::endl;
     print_array(array, array_size);
 
     // Merge two sorted arrays
     int first[6] = {1, 3, 5, 7, 10, 13};
     int second[7] = {-5, 0, 1, 2, 9, 10, 15};
     int *merged = merge_two_sorted_arrays(first, std::size(first), second, std::size(second));
-    std::cout << "Merged array: " << std::endl;
+    std::cout << "Merged array(Should be: [-5, 0, 1, 1, 2, 3, 5, 7, 9, 10, 10, 13, 15]): " << std::endl;
     print_array(merged, std::size(first) + std::size(second));
     delete[] merged;
 

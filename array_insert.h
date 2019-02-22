374cf08b7085d7417ce7800d2d11371deb48e43c
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-22 14:26:59 +0200

    Remove unnecessary declaration

--- array_insert.h
+++ array_insert.h
@@ -2,9 +2,6 @@
 #define ALCOTRIP_ARRAY_INSERT_H
 
 template<class T>
-void insert_subarray_into_array(T array[], int array_size, int insert_index, T sub_array[], int subarray_size);
-
-template<class T>
 void shift_array_elements_by(T array[], int array_size, int shift_by, int start_from);
 
 template<class T>

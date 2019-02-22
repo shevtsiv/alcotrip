fc747c43f0e7f355625b1a69f10faed6a68c9da8
shevtsiv <rostykshevtsiv@gmail.com>
2019-02-22 17:38:52 +0200

    Remove unnecessary declaration of print_array

--- utils.h
+++ utils.h
@@ -2,9 +2,6 @@
 #define ALCOTRIP_UTILS_H
 
 template<class T>
-void print_array(T array[], int array_size);
-
-template<class T>
 void print_array(T array[], int array_size) {
     std::cout << "[";
     for (int i = 0; i < array_size; i++) {
